973fa9d8f3034bdbab6a656bacd4e51c34132efa
Muhammad Arslan Shahzad <69626496+mas2000@users.noreply.github.com>
2021-10-13 03:39:58 +0500

    removed values and dummy values added

--- whatsapp_mern_clone/src/Firebase.js
+++ whatsapp_mern_clone/src/Firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAdbO0FSAvnadR7pwMZrb66Ze13sI2f1oA",
-    authDomain: "whatsapp-mern-9f785.firebaseapp.com",
-    projectId: "whatsapp-mern-9f785",
-    storageBucket: "whatsapp-mern-9f785.appspot.com",
-    messagingSenderId: "121136316626",
-    appId: "1:121136316626:web:faa4ab806aa6b8f4e4ed0b"
+    apiKey: "api_key here",
+    authDomain: "auth_domain.firebaseapp.com",
+    projectId: "project__id",
+    storageBucket: "storeage_bucket.appspot.com",
+    messagingSenderId: "MessageSenderID",
+    appId: "app_id"
   };
